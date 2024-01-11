cd1bd712e4349c821bd8e35bf92f3598603b5652
YdrMaster <ydrml@hotmail.com>
2024-01-11 10:48:03 +0800

    style: 修改 zip 使用，使对称的对象看起来对称
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,36 +1,26 @@
-﻿use std::ops::{Add, Div};
+﻿use std::iter::zip;
 
 pub(crate) fn rmsnorm(o: &mut [f32], x: &[f32], weight: &[f32]) {
     let ss = rmsnorm_reduce(x);
-    for ((o, &w), &x) in o.iter_mut().zip(x).zip(weight) {
-        *o = w * (ss * x);
-    }
+    zip(o, zip(x, weight)).for_each(|(o, (x, w))| *o = w * (ss * x));
 }
 
 pub(crate) fn rmsnorm_inplace(x: &mut [f32], weight: &[f32]) {
-    // ss = (Σx^2 / n + δ)^(-1/2)
     let ss = rmsnorm_reduce(x);
-    for (x, &w) in x.iter_mut().zip(weight) {
-        *x = w * (ss * *x);
-    }
+    zip(x, weight).for_each(|(x, w)| *x *= w * ss);
 }
 
 #[inline]
 fn rmsnorm_reduce(x: &[f32]) -> f32 {
     // (Σx^2 / n + δ)^(-1/2)
-    x.iter()
-        .map(|x| x * x)
-        .sum::<f32>()
-        .div(x.len() as f32)
-        .add(1e-5)
-        .sqrt()
-        .recip()
+    let y = x.iter().map(|x| x * x).sum::<f32>();
+    (y / (x.len() as f32) + 1e-5).powf(-0.5)
 }
 
 pub(crate) fn matmul(xout: &mut [f32], x: &[f32], w: &[f32]) {
     let n = x.len();
     xout.iter_mut().enumerate().for_each(|(i, y)| {
-        *y = w[i * n..].iter().zip(x).map(|(&w, &x)| w * x).sum::<f32>();
+        *y = zip(&w[i * n..], x).map(|(&w, &x)| w * x).sum::<f32>();
     });
 }
 
@@ -44,4 +34,9 @@ pub(crate) fn softmax(x: &mut [f32]) {
         })
         .sum::<f32>();
     x.iter_mut().for_each(|x| *x /= sum);
+}
+
+#[inline]
+pub(crate) fn sigmoid(x: f32) -> f32 {
+    1. / (1. + (-x).exp())
 }
--- src/transformer/mod.rs
+++ src/transformer/mod.rs
@@ -2,16 +2,12 @@
 mod weights;
 
 use super::{
-    kernel::{matmul, rmsnorm, rmsnorm_inplace, softmax},
+    kernel::{matmul, rmsnorm, rmsnorm_inplace, sigmoid, softmax},
     tokenizer::utok,
 };
 use config::Config;
 use memmap2::Mmap;
-use std::{
-    fs::File,
-    ops::{Add, Div, Neg},
-    path::Path,
-};
+use std::{fs::File, iter::zip, path::Path};
 use weights::Weights;
 
 /// `upos` for position id.
@@ -21,12 +17,6 @@ pub(super) type upos = u32;
 pub(super) struct Transformer {
     state: RunState,
     mmap: Mmap,
-}
-
-macro_rules! slice {
-    ($blob:expr; $width:expr; [$line:expr]) => {
-        $blob[$line * $width..][..$width]
-    };
 }
 
 impl Transformer {
@@ -48,6 +38,12 @@ impl Transformer {
     }
 
     pub fn forward(&mut self, token: utok, pos: upos) -> &mut [f32] {
+        macro_rules! slice {
+            ($blob:expr; $width:expr; [$line:expr]) => {
+                $blob[$line * $width..][..$width]
+            };
+        }
+
         let (config, _) = Config::map(&self.mmap);
         let w = Weights::new(&self.mmap);
         let s = &mut self.state;
@@ -63,7 +59,7 @@ impl Transformer {
         let head_size = config.head_size();
 
         let content_row = &slice!(w.token_embedding_table; dim; [token]);
-        s.x[..dim].copy_from_slice(content_row);
+        s.x.copy_from_slice(content_row);
 
         for l in 0..n_layer {
             rmsnorm(&mut s.xb, &s.x, &slice!(w.rms_att_weight; dim; [l]));
@@ -104,52 +100,42 @@ impl Transformer {
             let kv_mul = config.n_heads() / config.n_kv_heads();
             let div = (head_size as f32).sqrt();
 
+            s.xb.fill(0.);
             for h in 0..n_head {
-                let q = &mut slice!(q; head_size; [h]);
-                let att = &mut slice!(s.att; seq_len; [h]);
-                for (t, att) in att[..pos].iter_mut().enumerate() {
+                let q = &slice!(q; head_size; [h]);
+                let att = &mut s.att[..=pos];
+                for (t, a) in att.iter_mut().enumerate() {
                     let k = &slice!(s.key_cache; kv_dim; [l * seq_len + t]);
                     let k = &slice!(k; head_size; [h / kv_mul]);
-                    let score = q.iter().zip(k).map(|(&q, &k)| q * k).sum::<f32>().div(div);
-                    *att = score;
+                    let score = zip(q, k).map(|(&q, &k)| q * k).sum::<f32>() / div;
+                    *a = score;
                 }
 
-                softmax(&mut att[..pos + 1]);
+                softmax(att);
 
                 let xb = &mut slice!(s.xb; head_size; [h]);
-                xb.fill(0.);
-                for t in 0..=pos {
+                for (t, &a) in att.iter().enumerate() {
                     let v = &slice!(s.value_cache; kv_dim; [l * seq_len + t]);
                     let v = &slice!(v; head_size; [h / kv_mul]);
-                    let att = att[t];
-                    for (xb, &v) in xb.iter_mut().zip(v) {
-                        *xb += att * v;
-                    }
+                    zip(xb.iter_mut(), v).for_each(|(xb, &v)| *xb += a * v);
                 }
             }
 
             matmul(&mut s.xb2, &s.xb, &slice!(w.wo; dim * dim; [l]));
-
-            s.x.iter_mut().zip(&s.xb2).for_each(|(x, &xb2)| *x += xb2);
+            zip(&mut s.x, &s.xb2).for_each(|(x, &xb2)| *x += xb2);
 
             rmsnorm(&mut s.xb, &s.x, &slice!(w.rms_ffn_weight; dim; [l]));
 
             matmul(&mut s.hb, &s.xb, &slice!(w.w1; dim * hidden_dim; [l]));
             matmul(&mut s.hb2, &s.xb, &slice!(w.w3; dim * hidden_dim; [l]));
 
-            for (hb, hb2) in s.hb.iter_mut().zip(&s.hb2) {
-                *hb *= hb.neg().exp().add(1.).recip() * hb2;
-            }
+            zip(&mut s.hb, &s.hb2).for_each(|(hb, hb2)| *hb *= sigmoid(*hb) * hb2);
 
-            matmul(&mut s.xb, &s.hb, &slice!(w.w2; dim * hidden_dim; [l]));
-
-            for (x, xb) in s.x.iter_mut().zip(&s.xb) {
-                *x += xb;
-            }
+            matmul(&mut s.xb2, &s.hb, &slice!(w.w2; dim * hidden_dim; [l]));
+            zip(&mut s.x, &s.xb2).for_each(|(x, &xb2)| *x += xb2);
         }
 
         rmsnorm_inplace(&mut s.x, &w.rms_final_weight);
-
         matmul(&mut s.logits, &s.x, &w.wcls);
 
         return &mut s.logits;
@@ -157,14 +143,14 @@ impl Transformer {
 }
 
 struct RunState {
-    x: Vec<f32>,
-    xb: Vec<f32>,
-    xb2: Vec<f32>,
-    hb: Vec<f32>,
-    hb2: Vec<f32>,
-    q: Vec<f32>,
-    att: Vec<f32>,
-    logits: Vec<f32>,
+    x: Vec<f32>,      // no cache
+    xb: Vec<f32>,     // no cache
+    xb2: Vec<f32>,    // no cache
+    hb: Vec<f32>,     // no cache
+    hb2: Vec<f32>,    // no cache
+    q: Vec<f32>,      // no cache
+    att: Vec<f32>,    // no cache
+    logits: Vec<f32>, // no cache
     key_cache: Vec<f32>,
     value_cache: Vec<f32>,
 }
@@ -174,7 +160,6 @@ impl RunState {
         let dim = config.dim();
         let hidden_dim = config.hidden_dim();
         let n_layers = config.n_layers();
-        let n_heads = config.n_heads();
         let seq_len = config.seq_len();
         let kv_dim = config.kv_dim();
         Self {
@@ -186,7 +171,7 @@ impl RunState {
             q: vec![0.; dim],
             key_cache: vec![0.; n_layers * seq_len * kv_dim],
             value_cache: vec![0.; n_layers * seq_len * kv_dim],
-            att: vec![0.; n_heads * seq_len],
+            att: vec![0.; seq_len],
             logits: vec![0.; config.vocab_size()],
         }
     }
