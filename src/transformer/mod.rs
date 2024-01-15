7e676f19f8d87a6bf28c050c44d2d587ced1a849
YdrMaster <ydrml@hotmail.com>
2024-01-15 15:28:24 +0800

    style: 状态存储分离到独立文件方便进一步添加注释
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/transformer/mod.rs
+++ src/transformer/mod.rs
@@ -1,4 +1,5 @@
 ﻿mod config;
+mod state;
 mod weights;
 
 use super::{
@@ -7,6 +8,7 @@ use super::{
 };
 use config::Config;
 use memmap2::Mmap;
+use state::{Layer, RotaryEmbedder, RunState};
 use std::{fs::File, iter::zip, path::Path};
 use weights::Weights;
 
@@ -62,11 +64,8 @@ impl Transformer {
         let mut s = RunState::new(tok_len, config);
         let h = s.hidden.split_at_mut(tok_len * hidden_dim);
 
-        for (i, &token) in tokens.iter().enumerate() {
-            let token = token as usize;
-
-            let content_row = &slice!(w.token_embedding_table; dim; [token]);
-            slice!(s.x0; dim; [i]).copy_from_slice(content_row);
+        for (i, token) in tokens.iter().map(|&t| t as usize).enumerate() {
+            slice!(s.x0; dim; [i]).copy_from_slice(&slice!(w.token_embedding_table; dim; [token]));
         }
 
         for (l, layer) in self.layers.iter_mut().enumerate() {
@@ -248,10 +247,6 @@ impl Transformer {
             }
         }
 
-        if tok_len > 1 {
-            s.x0.copy_within((tok_len - 1) * dim.., 0);
-            s.x0.truncate(dim);
-        }
         s.x0
     }
 
@@ -280,79 +275,5 @@ impl Transformer {
         }
 
         &mut self.logits
-    }
-}
-
-/// This struct is new for each inference.
-struct RunState {
-    x0: Vec<f32>,
-    x1: Vec<f32>,
-    q: Vec<f32>,
-    hidden: Vec<f32>,
-    attention: Vec<f32>,
-}
-
-impl RunState {
-    fn new(tok_len: usize, config: &Config) -> Self {
-        let dim = config.dim();
-        Self {
-            x0: vec![0.; tok_len * dim],
-            x1: vec![0.; tok_len * dim],
-            q: vec![0.; tok_len * dim],
-            hidden: vec![0.; tok_len * config.hidden_dim() * 2],
-            attention: vec![0.; tok_len * config.n_heads() * config.seq_len()],
-        }
-    }
-}
-
-#[derive(Clone)]
-struct Layer {
-    k_cache: Vec<f32>,
-    v_cache: Vec<f32>,
-}
-
-impl Layer {
-    fn new(config: &Config) -> Self {
-        let len = config.seq_len() * config.kv_dim();
-        Self {
-            k_cache: vec![0.; len],
-            v_cache: vec![0.; len],
-        }
-    }
-}
-
-struct RotaryEmbedder {
-    dim: usize,
-    rotary: Vec<f32>,
-}
-
-impl RotaryEmbedder {
-    pub fn new(config: &Config) -> Self {
-        let dim = config.dim();
-        let n_heads = config.n_heads();
-        let seq_len = config.seq_len();
-        let head_size = dim / n_heads;
-        let mut rotary = Vec::with_capacity(seq_len * dim);
-        for pos in 0..seq_len {
-            for i in (0..dim).step_by(2) {
-                let freq = 1e4f32.powf(-((i % head_size) as f32 / head_size as f32));
-                let (sin, cos) = (pos as f32 * freq).sin_cos();
-                rotary.push(cos);
-                rotary.push(sin);
-            }
-        }
-        Self { dim, rotary }
-    }
-
-    fn run(&self, pos: usize, data: &mut [f32]) {
-        let rotary = &slice!(self.rotary; self.dim; [pos]);
-        for i in 0..data.len() / 2 {
-            let x = &mut slice!(data; 2; [i]);
-            let w = &slice!(rotary; 2; [i]);
-            x.copy_from_slice(&[
-                x[0] * w[0] - x[1] * w[1], //
-                x[1] * w[0] + x[0] * w[1],
-            ]);
-        }
     }
 }
---
+++ src/transformer/state.rs
@@ -0,0 +1,75 @@
+﻿use super::config::Config;
+use crate::kernel::slice;
+
+pub(super) struct RunState {
+    pub x0: Vec<f32>,
+    pub x1: Vec<f32>,
+    pub q: Vec<f32>,
+    pub hidden: Vec<f32>,
+    pub attention: Vec<f32>,
+}
+
+impl RunState {
+    pub fn new(tok_len: usize, config: &Config) -> Self {
+        let dim = config.dim();
+        Self {
+            x0: vec![0.; tok_len * dim],
+            x1: vec![0.; tok_len * dim],
+            q: vec![0.; tok_len * dim],
+            hidden: vec![0.; tok_len * config.hidden_dim() * 2],
+            attention: vec![0.; tok_len * config.n_heads() * config.seq_len()],
+        }
+    }
+}
+
+#[derive(Clone)]
+pub(super) struct Layer {
+    pub k_cache: Vec<f32>,
+    pub v_cache: Vec<f32>,
+}
+
+impl Layer {
+    pub fn new(config: &Config) -> Self {
+        let len = config.seq_len() * config.kv_dim();
+        Self {
+            k_cache: vec![0.; len],
+            v_cache: vec![0.; len],
+        }
+    }
+}
+
+pub(super) struct RotaryEmbedder {
+    dim: usize,
+    rotary: Vec<f32>,
+}
+
+impl RotaryEmbedder {
+    pub fn new(config: &Config) -> Self {
+        let dim = config.dim();
+        let n_heads = config.n_heads();
+        let seq_len = config.seq_len();
+        let head_size = dim / n_heads;
+        let mut rotary = Vec::with_capacity(seq_len * dim);
+        for pos in 0..seq_len {
+            for i in (0..dim).step_by(2) {
+                let freq = 1e4f32.powf(-((i % head_size) as f32 / head_size as f32));
+                let (sin, cos) = (pos as f32 * freq).sin_cos();
+                rotary.push(cos);
+                rotary.push(sin);
+            }
+        }
+        Self { dim, rotary }
+    }
+
+    pub fn run(&self, pos: usize, data: &mut [f32]) {
+        let rotary = &slice!(self.rotary; self.dim; [pos]);
+        for i in 0..data.len() / 2 {
+            let x = &mut slice!(data; 2; [i]);
+            let w = &slice!(rotary; 2; [i]);
+            x.copy_from_slice(&[
+                x[0] * w[0] - x[1] * w[1], //
+                x[1] * w[0] + x[0] * w[1],
+            ]);
+        }
+    }
+}
